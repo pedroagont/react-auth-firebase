5561c145fcd9a6b9a43f01eb5cdfba2637e56363
Pedro A. González <pgonzalez@post.com>
2021-08-23 17:00:34 -0500

    Import components, add routes config to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
-import Signup from './Signup'
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+import Login from './Login';
+
 import { AuthProvider } from '../contexts/authContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <AuthProvider>
-      <Signup />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Dashboard } />
+          <Route exact path="/signup" component={ Signup } />
+          <Route exact path="/login" component={ Login } />
+        </Switch>
+      </Router>
     </AuthProvider>
   );
 }
