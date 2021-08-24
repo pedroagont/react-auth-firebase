6624874d5b66889e07fa364ddd67a61136f5261d
Pedro A. González <pgonzalez@post.com>
2021-08-24 05:17:50 -0500

    Create and use Private Routes to redirect

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import Login from './Login';
 import { AuthProvider } from '../contexts/authContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './PrivateRoute';
+
 function App() {
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route exact path="/" component={ Dashboard } />
+          <PrivateRoute exact path="/" component={ Dashboard } />
           <Route exact path="/signup" component={ Signup } />
           <Route exact path="/login" component={ Login } />
         </Switch>
