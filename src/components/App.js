284417f34bf7849f0ad7661613222901721ca6be
Pedro A. González <pgonzalez@post.com>
2021-08-27 21:11:44 -0500

    Add private route to profile component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Dashboard from './Dashboard';
 import Login from './Login';
 import ForgotPassword from './ForgotPassword';
 import UpdateProfile from './UpdateProfile';
+import Profile from './Profile';
 
 import { AuthProvider } from '../contexts/authContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -16,6 +17,7 @@ function App() {
         <Switch>
           <PrivateRoute exact path="/" component={ Dashboard } />
           <PrivateRoute path="/update-profile" component={ UpdateProfile } />
+          <PrivateRoute path="/profile" component={ Profile } />
           <Route path="/signup" component={ Signup } />
           <Route path="/login" component={ Login } />
           <Route path="/forgot-password" component={ ForgotPassword } />
