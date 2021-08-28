ab46638720565a8f26267540ec4dac0b81876394
Pedro A. González <pgonzalez@post.com>
2021-08-27 21:11:28 -0500

    Add navigationbar, show displayname or email to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,17 @@
-import Profile from './Profile';
+import NavigationBar from './NavigationBar';
+import { useAuth } from '../contexts/authContext';
 
 function Dashboard() {
+  const { currentUser } = useAuth();
 
   return (
     <>
-      <h2 className="display-4 text-center pt-5">My dashboard 🕹</h2>
-      <Profile />
+      <NavigationBar />
+      {
+        currentUser.displayName
+        ? <h2 className="display-4 text-center pt-5">Welcome, { currentUser.displayName } 👋</h2>
+        : <h2 className="display-4 text-center pt-5">Welcome, { currentUser.email } 👋</h2>
+      }
     </>
   );
 }
