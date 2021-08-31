2b920e00c3e220951ad00438c0430f3151749400
Pedro A. González <pgonzalez@post.com>
2021-08-31 17:14:22 -0500

    Import, initialize firebase storage service and export

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,5 +13,6 @@ const firebaseConfig = {
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAuth = firebaseApp.auth();
+const firebaseStorage = firebaseApp.storage()
 
-export { firebaseApp, firebaseAuth };
+export { firebaseAuth, firebaseStorage };
